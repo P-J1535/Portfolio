2fbf71a3616845b9d9656b4218aa040c66b4c147
Prathamesh <hello.ravinderkumar@gmail.com>
2024-05-23 17:21:28 +0530

    updated Code

--- portfolio/src/pages/Resume/Resume.js
+++ portfolio/src/pages/Resume/Resume.js
@@ -3,63 +3,83 @@ import styles from './Resume.module.css';
 import { Link } from 'react-router-dom';
 
 const Resume = () => {
+  const resumeData = [
+    {
+      id: 1,
+      year: '2023 - 2024',
+      institude: 'WORK EXPERIENCE',
+      location: 'Front- End Developer KJSS Comsos',
+      description:''
+    },
+    {
+      id: 2,
+      year: '2022 - 2023',
+      institude: 'SPRK Technologies Institute',
+      location: 'Internship Completed',
+      description:'',
+    },
+    {
+      id: 6,
+      year: '2021 - 2022',
+      institude: 'Join SPRk Technologies Institude',
+      location: `I'm grateful to be part of this institute. Even during the lockdown, they ensured we received all services, conducted online lectures and webinars, and resumed offline batches with necessary precautions as per government norms. It's been a great experience!`,
+      description:'',
+    },
+    {
+      id: 3,
+      year: '2018 - 2021',
+      institude: 'Bachelor of Science in Information Technology',
+      location: 'Ramsheth Thakur College, Kharghar, Mumbai University',
+      description:'',
+    },
+    {
+      id: 4,
+      year: '2016 - 2018',
+      institude: 'Higher Secondary Certificate (HSC)',
+      location: 'Atmaram Dhodu Mhatre Juinior College Nawde',
+      description:'',
+    },
+    {
+      id: 5,
+      year: '2014 - 2016',
+      institude: 'Secondary School Certificate (SSC)',
+      location: 'TRimurti Vidya Mandir Dahisar Mori',
+      description:'',
+    },
+   
+    
+  ]
   return (
     <div className={styles.mainBox}>
-    <div className={styles.resume}>
-      <h1>My Resume</h1>
-      <p>
-      Experienced React Developer adept at creating and managing web-based software solutions. Skilled in JavaScript and
-      React with 1.5 years of practical experience.
-      </p>
-      <div className={styles.grid}>
-      <div className={styles.card}>
-          <h2>2023-2024</h2>
-          <p>WORK EXPERIENCE</p>
-          <h6>Front- End Developer 
-          KJSS Comsos </h6>
+      <div className={styles.resume}>
+        <h1>Resume</h1>
+        <h3>Resume</h3>
+        <p>
+          Experienced React Developer adept at creating and managing web-based software solutions. Skilled in JavaScript and
+          React with 1.5 years of practical experience.
+        </p>
+        <div className={styles.grid}>
+          {resumeData.map(resume => (
+            <div className={styles.card} key={resume.id}>
+              <h2>{resume.year}</h2>
+              <p>{resume.institude}</p>
+              <h6>{resume.location}</h6>
+
+            </div>
+          ))}
+
         </div>
-        <div className={styles.card}>
-        <h2>2022-2023</h2>
-        <p>SPRK Technologies Institute</p>
-        <h6>Internship Completed</h6>
+        <Link to="/cv"
+          style={{
+            display: 'flex',
+            justifyContent: 'center'
+          }}
+        >
+          <button className={styles.downloadButton}>Download CV</button>
+        </Link>
       </div>
-        <div className={styles.card}>
-        <h2>2021-2022</h2>
-        <p>SPRK Technologies Institute</p>
-        <h6>Mern Full-Stack Cource Completed</h6>
-      </div>
-        <div className={styles.card}>
-          <h2>2018-2021</h2>
-          <p>Bachelor of Science in Information
-          Technology</p>
-          <h6>Ramsheth Thakur College, Kharghar, Mumbai University</h6>
-        </div>
-        <div className={styles.card}>
-          <h2>2016-2018</h2>
-          <p>Higher Secondary Certificate (HSC) </p>
-          <h6>
-          
-Atmaram Dhodu Mhatre Jr College, Nawade 
-          </h6>
-        </div>
-        <div className={styles.card}>
-          <h2>2014-2016</h2>
-          <p>Secondary School Certificate (SSC) 
-          </p>
-          <h6>Trimurti Vidya Mandir, Dahisar Mori</h6>
-        </div>
-        
-     
-      </div>
-      <Link to="/cv" 
-      style={{
-        display:'flex',
-        justifyContent:'center'
-      }}
-      >
-        <button className={styles.downloadButton}>Download CV</button>
-      </Link>
-    </div>
+
+  
     </div>
   );
 };
