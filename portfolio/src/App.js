03a94ca43ae2a5c4cbce8d978bb44a40a1624f08
prathamesh <prathameshjepal97@gmail.com>
2024-05-26 01:49:04 +0530

    updated Code

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,21 +1,22 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Layout from './pages/Layout/Layout';
-import Resume from './pages/Resume/Resume';
-import CV from './pages/CV/CV';
-import NavbarLite from './components/NavbarLite/NavbarLite';
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
+import CV from './pages/CV/CV'
+import ContactForm from './pages/ContactForm/ContactForm'
+import ContactUs from './pages/ContactUs/ContactUs'
+import Resume from './pages/Resume/Resume'
+import HomePage from './pages/HomePage/HomePage'
 
 const App = () => {
   return (
-    <BrowserRouter>
-  
-      <Routes>
-        <Route index element={<Resume /> } />
-         <Route path='/cv' element={<CV />} />
-      </Routes>
-       
-    </BrowserRouter>
-  );
-};
+    <div>
+    <Navbar />
+    <HomePage />
+    <ContactUs />
+    <ContactForm />
+    <Footer />
+    </div>
+  )
+}
 
-export default App;
+export default App
--- portfolio/src/pages/ContactUs/ContactUs.js
+++ portfolio/src/pages/ContactUs/ContactUs.js
@@ -11,36 +11,39 @@ const ContactUs = () => {
     return (
         <div className={styles.contactUs}>
             <div className={styles.contactPara}>
+            <h1>Contact Me</h1>
+            <h2>Contact</h2>
+            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia
+            </p>
+
             </div>
 
             <div className={styles.contactMenu}>
                 <div className={styles.contactBox}>
-                    <HomeIcon 
-                    sx={{
-                        borderRadius:'50%',
-                        padding:'20px',
-                        backgroundColor:'gray',
-                        
-                    }}
+                    <HomeIcon className={styles.icons} />
+                    <h3>ADDRESS</h3>
+                    <p>Kharghar, Navi-Mumbai</p>
+                </div>
+
+                <div className={styles.contactBox}>
+                    <HomeIcon
+                        className={styles.icons}
+
                     />
-                    <h3>ADDRESS</h3>
-                    <p>198 West 21th Street, Suite 721 New York NY 10016</p>
+                    <h3>Contact Number</h3>
+                    <p>+91 8451031535</p>
                 </div>
-                <div>
-                    <HomeIcon />
-                    <h3>ADDRESS</h3>
-                    <p>198 West 21th Street, Suite 721 New York NY 10016</p>
+                <div className={styles.contactBox}>
+                    <HomeIcon className={styles.icons} />
+                    <h3>Email Address</h3>
+                    <p>Prathameshjepal97@gmail.com</p>
                 </div>
-                <div>
-                    <HomeIcon />
-                    <h3>ADDRESS</h3>
-                    <p>198 West 21th Street, Suite 721 New York NY 10016</p>
+                <div className={styles.contactBox}>
+                    <HomeIcon className={styles.icons} />
+                    <h3>Website</h3>
+                    <p>www.attedenceportal.com</p>
                 </div>
-                <div>
-                    <HomeIcon />
-                    <h3>ADDRESS</h3>
-                    <p>198 West 21th Street, Suite 721 New York NY 10016</p>
-                </div>
+
 
             </div>
 
